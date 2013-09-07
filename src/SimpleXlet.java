16934773cbd0b22c4b5433575fb525e39791f804
nicolas moraes <nico.moraes@gmail.com>
2013-09-07 15:52:59 -0300

    captura de eventos del control

--- src/SimpleXlet.java
+++ src/SimpleXlet.java
@@ -23,19 +23,30 @@ public class SimpleXlet implements Xlet, KeyListener {
     private XletContext context;
     private HScene scene;
     private HStaticText label;
-    private Color[] colors = { Color.black, Color.red, Color.blue };
+    private Color[] colors = { Color.black, Color.red, Color.blue , Color.CYAN };
     private int intColor;
+    
+	// this object is responsible for displaying the background I-frame
+	private HaviBackgroundController backgroundManager;
+    
 
     public SimpleXlet() {
     }
 
     public void initXlet(XletContext xletContext) throws XletStateChangeException {
-        System.out.println("begin initXlet");
+        System.out.println("Ejecutando initXlet...");
         context = xletContext;
     }
 
     public void startXlet() throws XletStateChangeException {
-        System.out.println("begin startXlet");
+        System.out.println("Ejecutando startXlet...");
+    	// This adds the background image to the display.  The background image is
+		// displayed in the background plane.
+		displayBackgroundImage();
+		
+		// The bitmap image is shown in the graphics plane.
+		displayForegroundBitmap();
+        
         HSceneFactory hsceneFactory = HSceneFactory.getInstance();
         scene = hsceneFactory.getFullScreenScene(HScreen.getDefaultHScreen().getDefaultHGraphicsDevice());
 
@@ -69,17 +80,103 @@ public class SimpleXlet implements Xlet, KeyListener {
     public void keyReleased(KeyEvent e) {
     }
 
-    public void keyPressed(KeyEvent e) {
-    	
-    	System.out.println(e.getKeyChar());
+	public void keyPressed(KeyEvent key) {
+		// What key has been pressed?
+		switch (key.getKeyCode()) {
+		// if we have the 'up' key...
+		case KeyEvent.VK_UP: {
+			System.out.println("arriba ...");
+			break;
+		}
+		case KeyEvent.VK_DOWN: {
+			System.out.println("abajo ...");
 
+			break;
+		}
+
+		case KeyEvent.VK_LEFT: {
+			System.out.println("izquierda ...");
+
+			break;
+		}
+
+		case KeyEvent.VK_RIGHT: {
+			System.out.println("derecha ...");
+
+			break;
+		}
+
+		case 403: {
+			System.out.println("rojo ...");
+
+			break;
+		}
+		case 404: {
+			System.out.println("verde ...");
+
+			break;
+		}
+		case 405: {
+			System.out.println("amarillo  ...");
+
+			break;
+		}
+		case 406: {
+			System.out.println("azul ...");
+
+			break;
+		}
+
+		default: {
+			// do nothing
+			System.out.println("default case ...");
+			System.out.println(key);
+			break;
+		}
+		}
+	}
     	
+    	
+    	
+    	/*System.out.println(e.getKeyChar());
+    	    	
         intColor++;
         if (intColor == colors.length) {
             intColor = 0;
         }
         label.setBackground(colors[intColor]);
         label.repaint();
-    }
+        
+        
+        */
+   
+    
+	/**
+	 * This method is responsible for displaying the background image
+	 * on the screen.  This image will be displayed in the background
+	 * plane, not the graphics plane.  See the graphics tutorial for
+	 * a discussion of the differences between the two planes and how
+	 * they work together.
+	 */
+	public void displayBackgroundImage() {
+		// We've defined a separate object for displaying and managing
+		// the background image.
+		backgroundManager = new HaviBackgroundController();
+
+		// If we can initialise the background manager (which means we have got
+		// the resources we need to display the image), we load and display an
+		// image in the background plane.
+		if (backgroundManager.init()) {
+			backgroundManager.display("sky.jpg");
+		}
+	}
+    
+	/**
+	 * Display the foreground image in the AWT component
+	 */
+	public void displayForegroundBitmap()
+	{
+	}
+    
 
 }
