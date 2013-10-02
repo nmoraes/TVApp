0394aea41ba69491646e67d786ce1a42e55de4b3
agalli <alejandrodgalli@gmail.com>
2013-10-01 21:25:36 -0300

    mas de twitter

--- src/twitter/Cliente.java
+++ src/twitter/Cliente.java
@@ -1,7 +1,10 @@
 package twitter;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import persistencia.Persistir;
 
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -19,30 +22,43 @@ public class Cliente {
 		cb.setDebugEnabled(true)
 				.setOAuthConsumerKey("kEgC1K5vx2mvRYxePYCUA")
 				.setOAuthConsumerSecret(
-						"B1TQEaekHjW9XRBDSC3cTBKXx5EMaWiwffS4NrGiqfQ")
-				.setUser("TVDigital1").setPassword("ihome01");
-		TwitterFactory tf=new TwitterFactory(cb.build());
-		Twitter twitter= tf.getInstance();
-		
-		RequestToken token=twitter.getOAuthRequestToken();
-		System.out.println(token.getAuthorizationURL());
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		
-		String pin=bf.readLine();
+						"B1TQEaekHjW9XRBDSC3cTBKXx5EMaWiwffS4NrGiqfQ");
+				//.setUser("TVDigital1").setPassword("ihome01");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		Persistir persistir = new Persistir();
+		Usuario usuario = persistir.leerUsuarioTwitter();
 		AccessToken access;
-		if (pin.length() > 0) {
-			access = twitter.getOAuthAccessToken(token, pin);
-        } else {
-        	access = twitter.getOAuthAccessToken(token);
-        }
-		//AccessToken access=twitter.getOAuthAccessToken(token, pin);
+		if (usuario == null) {
+			RequestToken token = twitter.getOAuthRequestToken();
+			System.out.println(token.getAuthorizationURL());
+			BufferedReader bf = new BufferedReader(new InputStreamReader(
+					System.in));
+
+			String pin = bf.readLine();
+			if (pin.length() > 0) {
+				access = twitter.getOAuthAccessToken(token, pin);
+			} else {
+				access = twitter.getOAuthAccessToken(token);
+			}
+			Usuario usuario2=new Usuario();
+			usuario2.setId("");
+			usuario2.setToken(access.getToken());
+			usuario2.setSecretToken(access.getTokenSecret());
+			persistir.persistirUsuarioTwitter(usuario2);
+			System.out.println("usuario nuevo");
+		}else{
+			access=new AccessToken(usuario.getToken(), usuario.getSecretToken());
+			twitter.setOAuthAccessToken(access);
+			System.out.println("existe usuario");
+		}
+		// AccessToken access=twitter.getOAuthAccessToken(token, pin);
 		System.out.println(access.getToken());
 		System.out.println("publicando");
-		String mensaje="probando app para tv digital 5";
-		Status status=twitter.updateStatus(mensaje);
+		String mensaje = "probando app para tv digital";
+		Status status = twitter.updateStatus(mensaje);
 		System.out.println("publicado");
 		System.out.println(status.getText());
-		
 
 	}
 }
