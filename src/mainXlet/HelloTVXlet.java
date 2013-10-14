0092e6f91fd7fe5bee4815f34f3ef8800a6fee60
nicolas moraes <nico.moraes@gmail.com>
2013-10-13 23:19:46 -0200

    comento la fotito del medio porque esta molestando

--- src/mainXlet/HelloTVXlet.java
+++ src/mainXlet/HelloTVXlet.java
@@ -328,7 +328,7 @@ public class HelloTVXlet extends HComponent implements Xlet, Runnable, KeyListen
         // Create a MediaTracker to tell us when the image has loaded   
         MediaTracker tracker = new MediaTracker(this);   
         // Then load the image   
-        image = Toolkit.getDefaultToolkit().getImage("bg2.png");   
+       // image = Toolkit.getDefaultToolkit().getImage("bg2.png");   
              	
         
         // add the image to the MediaTracker...   
