533935af6174b2df7dd85cc82a810c272a9de610
Gabriel Casas <gcasas@correo.com.uy>
2013-10-17 21:57:51 -0200

    Subo la Calculadora a ver si va...

---
+++ src/Calc/base.java
@@ -0,0 +1,22 @@
+package Calc;
+
+
+public class base {
+
+ public double suma(double a,double b){
+ double r=a+b;
+ return r;
+ }
+ public double resta(double a,double b){
+ double r=a-b;
+ return r;
+ }
+ public double mul(double a,double b){
+ double r=a*b;
+ return r;
+ }
+ public double div(double a,double b){
+ double r=a/b;
+ return r;
+ }
+ }
