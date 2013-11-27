2fd5ea2bf640ed0c4589603d5771881bd39d026c
nicolas moraes <nico.moraes@gmail.com>
2013-11-27 13:03:28 -0200

    cosas

--- src/mainXlet/ContenedorTwitter.java
+++ src/mainXlet/ContenedorTwitter.java
@@ -1,7 +1,6 @@
 package mainXlet;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -11,14 +10,9 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.havi.ui.HContainer;
 import org.havi.ui.HMultilineEntry;
-import org.havi.ui.HSinglelineEntry;
-import org.havi.ui.HState;
 import org.havi.ui.HStaticText;
-import org.havi.ui.HVisible;
-
 import twitter.FachadaTwitter;
 import twitter.Tweet;
 
@@ -29,44 +23,33 @@ public class ContenedorTwitter extends HContainer implements KeyListener {
 	 */
 	private static final long serialVersionUID = 5292849366437972565L;
 
-	HStaticText hstTexto;
+	private HStaticText hstTexto;
 	private String tweet;
-	HStaticText nuevoTweet;
-	//(int x, int y, int width, int height, int maxChars)
-	HMultilineEntry multiEntrada;
+	private HStaticText nuevoTweet;
+	private HMultilineEntry multiEntrada;
 	
     // The image that we will show   
     private Image image;
-	
-    private String timeline;
-
-    private ArrayList <String>array= new ArrayList <String>();
-    
+    private ArrayList <String>array= new ArrayList <String>();    
     public static boolean escribir = false;
 
 	@SuppressWarnings("unchecked")
 	public ContenedorTwitter() {
 
-		tweet="";
-		//timeline=obtenerTweets();
-		array=obtenerTweets();
-		//hstTexto = new HStaticText(timeline);
-		//hstTexto.setFont(new Font("Tiresias", 0, 14));
-		// texto tamaño y posición x,y,x,y
-		//hstTexto.setBounds(100, 350, 400, 200);
-		//hstTexto.setBackground(Color.white);
-		//this.add(hstTexto);
+		tweet = "";
+		array = obtenerTweets();
+
 		nuevoTweet = new HStaticText("Rojo para escribir nuevo tweet");
 		nuevoTweet.setFont(new Font("Tiresias", 0, 14));
 		// texto tamaño y posición x,y,x,y
 		nuevoTweet.setBounds(10, 110, 350, 100);
 		nuevoTweet.setBackground(Color.white);
 		this.add(nuevoTweet);
-		// mi tamaño y posición x,y,x,y
 		loadForegroundBitmap();
-		
+
 		this.setBounds(0, 0, 800, 600);
 		this.addKeyListener(this);
+
 	}
 	
 	
@@ -78,7 +61,6 @@ public class ContenedorTwitter extends HContainer implements KeyListener {
 			escribir = true;
 			MainXlet.miniWeather.setVisible(false);
 			MainXlet.keyboard.setVisible(true);
-			timeline="";
 			MainXlet.keyboard.requestFocus();	
 			ContenedorKeyboard.invokeFather=Constant.CONTENEDOR_TWITTER;
 			array.set(0, "");
@@ -113,7 +95,6 @@ public class ContenedorTwitter extends HContainer implements KeyListener {
 	private ArrayList obtenerTweets() {
 		FachadaTwitter ft = new FachadaTwitter();
 		List lista = ft.getHomeTimeLine();
-		//StringBuilder tl = new StringBuilder();
 		for (int i = 0; i < 5; i++) {
 			Tweet tweet = (Tweet) lista.get(i);			
 			String tw=tweet.getUsuario() + ": " + tweet.getMensaje();
@@ -130,29 +111,19 @@ public class ContenedorTwitter extends HContainer implements KeyListener {
 	}
 	
 	
-	 public void loadForegroundBitmap() {   
-	        // Create a MediaTracker to tell us when the image has loaded   
-	        MediaTracker tracker = new MediaTracker(this);   
-	        // Then load the image   
-	       image = Toolkit.getDefaultToolkit().getImage("textTwitter.png");   
-	             	
-	        
-	        // add the image to the MediaTracker...
-	       // tracker.addImage(image, 0);
-	        tracker.addImage(image, 0);
-	        
-	        
-	        // ...and wait for it to finish loading   
-	        try{   
-	            tracker.waitForAll();   
-	        }   
-	        catch(InterruptedException e) {   
-	            // Ignore the exception, since there's not a lot we can do.   
-	            image = null;
-	        
-	        }   
-	    }  
-	 
+	public void loadForegroundBitmap() {
+
+		MediaTracker tracker = new MediaTracker(this);
+		image = Toolkit.getDefaultToolkit().getImage("textTwitter.png");
+		tracker.addImage(image, 0);
+
+		try {
+			tracker.waitForAll();
+		} catch (InterruptedException e) {
+			image = null;
+
+		}
+	}
 	
 	 public void paint(Graphics graphics) {   
 		
@@ -162,20 +133,23 @@ public class ContenedorTwitter extends HContainer implements KeyListener {
 		        // Set the text colour   
 		        graphics.setColor(Color.white);
 			
+		    //desplegamos el timeline.
 			graphics.drawString(array.get(0), 12, 300);
 			graphics.drawString(array.get(1), 12, 315);
 			graphics.drawString(array.get(2), 12, 330);
 			graphics.drawString(array.get(3), 12, 345);
 			graphics.drawString(array.get(4), 12, 360);
 			
-			graphics.setColor(Color.black);
-
+			
 			if (image != null && escribir == true) {
+				graphics.setColor(Color.black);
 				graphics.drawImage(image, 150, 230, null);   
 				graphics.setFont(new Font("Tiresias", Font.PLAIN, 15));
 				graphics.drawString(ContenedorKeyboard.message, 158, 245);
 
 			}
+			
+	
 		} catch (Throwable t) {
 
 			System.out.println("error al escribir");
